cf4c618b7b5393ff16e63c564d544f59645bafd2
Kanishk Jain <kanishkjain0206@gmail.com>
2023-10-26 11:53:55 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# react-password-generator
- 
+
+# Password Generator
+
+This password generator app, built using React and various web technologies, provides users with a straightforward yet powerful tool for enhancing online security. It allows users to create random and secure passwords, offering customization options for password length and character types. The app features a user-friendly interface with theme toggling and easy copying to the clipboard. Additionally, it includes a password strength checker, utilizing the zxcvbn library, to evaluate and provide feedback on the security of generated passwords. 
+
+
+
+
+## Authors
+
+- [@kanishkjain0263](https://www.github.com/kanishkjain0263)
+
+
+## Run Locally
+
+To deploy this project run
+
+```bash
+  npm run deploy
+```
+
+1. Fort the code to your local machine.
+2. Navigate into the project directory
+```bash
+    cd password-generator
+```
+3. ReactJs Installation (Make sure you already installed Node)
+```bash
+    npm install
+```
+4. Run the program
+```bash
+    npm run start
+```
+## Features
+
+- Customizable critera for each random generated password
+- Theme toggle (light/dark)
+- Copy to clipboard
+- Password strength assessment
---
+++ password-generator-main/src/password.js
@@ -0,0 +1,47 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const capitalAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const num = '0123456789'
+const symbols = `!@#$%^&*`
+
+export default function generatePassword(length, setting) {
+    let password = '';
+    const listOfTypes = [];
+    if (setting.lowercase === true)
+        listOfTypes.push("al");
+    if (setting.upperCase === true)
+        listOfTypes.push("up_al");
+    if (setting.symbol === true)
+        listOfTypes.push("sym");
+    if (setting.number === true)
+        listOfTypes.push("num");
+
+    // generating password of <length> digits
+    for (let i = 0; i <= length - 1; i++) {
+
+        // alphabet or number
+
+        const type = listOfTypes[Math.floor(Math.random() * listOfTypes.length)];
+
+        if (type === "al") {
+            password += pickRandomElement(alphabet);
+        }
+        else if (type === "num") {
+            password += pickRandomElement(num);
+        }
+        else if (type === "up_al") {
+            password += pickRandomElement(capitalAlpha);
+        }
+        else if (type === "sym") {
+            password += pickRandomElement(symbols);
+        }
+
+
+    }
+
+    return password;
+}
+
+function pickRandomElement(parameter) {
+    const r = parameter[Math.floor(Math.random() * parameter.length)];
+    return r
+}
